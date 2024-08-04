dc7a7774af89dcea94503b0994165ce9f6530a75
unknown <ricardodonado1105@gmail.com>
2024-08-04 16:18:15 -0500

    all logic scripted, also made it responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-console.log("hello world!")
+const adviceNumberIDElement = document.getElementById("adviceID")
+const adviceContentElement = document.getElementById("adviceMessage")
+const reloadButton = document.getElementsByTagName("button")[0]
+
+function newAdvice(){
+    fetch('https://api.adviceslip.com/advice')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        adviceNumberIDElement.innerHTML = data.slip.id
+        adviceContentElement.innerHTML = data.slip.advice
+      })
+    .catch(error => console.error('Error al cargar el JSON:', error))
+}
+newAdvice()
+
+reloadButton.addEventListener("click", ()=>{
+    newAdvice()
+})
